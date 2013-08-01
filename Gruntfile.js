16e9d461cb1449ebc522266f5f19c8a36c4eb6de
Julian Krispel-Samsel <julian@goodafternoon.co>
2013-08-01 07:05:15 +0100

    Added readme for build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,6 @@
 module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.initConfig({
         coffee:{
             compileJoined: {
@@ -10,9 +11,15 @@ module.exports = function(grunt){
                     'js/main.js': 'coffee/*.coffee' // concat then compile into single file
                 }
             },
+        },
+        watch: {
+            coffee: {
+                files: ['coffee/*.coffee'],
+                tasks: 'coffee'
+            }
         }
     });
     grunt.registerTask('default',[
-        'coffee'
+        'coffee', 'watch'
     ]);
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+#Painter
+
+- To build run `npm run-script build` in your terminal. This will install all dependencies including CoffeeScript
+- To watch files and compile all CoffeeScript files, run `grunt`
